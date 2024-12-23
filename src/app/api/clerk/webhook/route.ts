47e557f1b207d7fa206d72c82fc6a5a683d4fb8c
Sushanth Nandeti <sushanthnandeti96@gmail.com>
2024-12-22 23:36:45 -0500

    Changes to Nexjs-Config file

--- src/app/api/clerk/webhook/route.ts
+++ src/app/api/clerk/webhook/route.ts
@@ -1,15 +1,34 @@
 // src/app/api/clerk/webhook/route.ts
 
+import { db } from "@/server/db";
+
 export async function POST(req: Request) {
+
     try {
+
         const { data } = await req.json();
+        const emailAddress  = data.email_addresses[0].email_address;
+        const firstName  = data.first_name; 
+        const lastName = data.last_name;
+        const imageUrl = data.image_url;
+        const id = data.id;
+
+        await db.user.create({
+            data: {
+            id: id,
+            emailAddress: emailAddress,
+            firstName: firstName,
+            lastName: lastName,
+            imageUrl: imageUrl,
+            }
+        })
 
         console.log("Clerk webhook received", data);
-
         return new Response("Webhook received", { status: 200 });
+
     } catch (error) {
         console.error("Error processing webhook:", error);
-
         return new Response("Error processing webhook", { status: 500 });
+
     }
 }
