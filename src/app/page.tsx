69add6185fb07eaac6ebc34b4a73ce6b01879b56
Sushanth Nandeti <sushanthnandeti96@gmail.com>
2024-12-22 19:58:24 -0500

    Shadcn UI Components added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
+import { Button } from "@/components/ui/button";
+
 
 export default async function Home() {
   
   return(
+    <>
     <h1> Hello </h1>
+    <Button> Click me </Button>
+    </>
   )
 }
