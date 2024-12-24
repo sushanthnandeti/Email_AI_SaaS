5da711e860cff5399dde296ea89308ea139a90e1
Sushanth Nandeti <sushanthnandeti96@gmail.com>
2024-12-23 23:04:53 -0500

    Clerk Bug fix

--- src/app/api/aurinko/callback/route.ts
+++ src/app/api/aurinko/callback/route.ts
@@ -1,10 +1,55 @@
-import { auth } from "@clerk/nextjs/server"
-import { NextResponse } from "next/server";
+import { exchangeCodeForToken,  getAccountDetails } from "@/lib/aurinko";
+import { auth, EmailAddress } from "@clerk/nextjs/server"
+import { NextResponse, NextRequest } from "next/server";
+import { db } from "@/server/db";
 
-
-export const GET = async(req: Request) => {
+export const GET = async(req: NextRequest) => {
 
     const {userId} = await auth();
-    console.log("User is: ", userId);
-    return NextResponse.json({message: "Hello, World!"});
-}
+
+    if (!userId) throw NextResponse.json({ message : "unauthorized"}, {status: 401})
+    
+    const params = req.nextUrl.searchParams
+    const status = params.get('status')   
+
+
+    if(status != 'success') return NextResponse.json({ message: 'Failed to link account'}, {status : 401})
+
+    // get the code to exchange for access token 
+
+        const code = params.get('code');
+
+        console.log("code is :", code);
+
+        if(!code) return NextResponse.json({ message: 'Failed to link account'}, {status : 401})
+
+        const token = await exchangeCodeForToken(code);
+
+        if(!token) return NextResponse.json({ message: 'Failed to exchange for access token'}, {status : 401})
+        
+    // get the account details by exchanging the token 
+
+        const accountDetails = await getAccountDetails(token.accessToken);
+        
+        console.log(token.accessToken);
+        console.log(accountDetails);
+        
+        await db.account.upsert({
+            where : {
+                id: token.accountId.toString()
+            },
+            update: {
+                accessToken : token.accessToken,
+            },
+            create : {
+                id: token.accountId.toString(),
+                userId, 
+                emailAddress : accountDetails.email,
+                name: accountDetails.name, 
+                accessToken : token.accessToken, 
+            }
+        }
+        )
+        
+        return NextResponse.redirect(new URL('/mail', req.url))
+    };
--- src/lib/aurinko.ts
+++ src/lib/aurinko.ts
@@ -1,6 +1,8 @@
 "use server"
 
 import { auth } from "@clerk/nextjs/server"
+import axios from "axios";
+
 
 export const getAurinkoAuthUrl = async(serviceType : 'Google' | 'Office365') => {
 
@@ -17,4 +19,57 @@ export const getAurinkoAuthUrl = async(serviceType : 'Google' | 'Office365') => 
     })
 
     return `https://api.aurinko.io/v1/auth/authorize?${params.toString()}`
+}
+
+
+export const exchangeCodeForToken = async( code: string) => {
+
+    try {
+        const response = await axios.post(`https://api.aurinko.io/v1/auth/token/${code}`, {}, {
+            auth: {
+                username : process.env.AURINKO_CLIENT_ID as string,
+                password : process.env.AURINKO_CLIENT_SECRET as string,
+            }
+        })
+
+        return response.data as {
+            accountId : number,
+            accessToken : string,
+            userId : string, 
+            userSession : string
+        }
+
+    } catch (error) {
+        if (axios.isAxiosError(error)) {
+            console.error(error.response?.data);
+        }
+        console.error(error); 
+    }
+}
+
+
+export const getAccountDetails = async(accessToken : string) => {
+    try {
+        const response = await axios.get('https://api.aurinko.io/v1/account', {
+
+            headers : {
+                'Authorization' : `Bearer ${accessToken}`
+            }
+       
+        })
+            console.log("The response is :", response);
+            return response.data as {
+                email: string 
+                name: string
+            }
+        
+    } catch (error) {
+
+        if(axios.isAxiosError(error)) {
+            console.error('Error fetching account details', error.response?.data);
+        } else { 
+            console.error('Unexpected error fetching account details', error)
+        }
+        throw error;
+    }
 }
