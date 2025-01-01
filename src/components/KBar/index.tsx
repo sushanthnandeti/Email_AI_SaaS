79c75ae20866a15f35b1a08328bec9e5d035a83d
Sushanth Nandeti <sushanthnandeti96@gmail.com>
2025-01-01 02:38:55 -0500

    KBar Search Features Implementation 1.0

--- src/components/KBar/index.tsx
+++ src/components/KBar/index.tsx
@@ -1,11 +1,75 @@
 "use client"
 
 import React from 'react'
-import {KBarProvider, KBarPortal, KBarAnimator, KBarSearch, KBarPositioner} from 'kbar';
+import {KBarProvider, KBarPortal, KBarAnimator, KBarSearch, KBarPositioner, Action, Priority} from 'kbar';
+import RenderResults from './render-results';
+import { useLocalStorage } from 'usehooks-ts';
 
 
 export default function KBar({ children } : {children : React.ReactNode}) {
-    return <KBarProvider>
+
+    const [_, setTab] = useLocalStorage('normalhuman-tab', 'inbox')
+    const [done, setDone] = useLocalStorage('normalhuman-done', false)
+
+    const actions: Action[] = [
+        {
+            id : 'inboxAction',
+            name: "Inbox",
+            shortcut: ['g','i'],
+            section: 'Navigation',
+            subtitle : "View your inbox",
+            perform: () => {
+                setTab('inbox')
+            }
+        },
+        {
+            id: "draftsAction",
+            name: "Drafts",
+            shortcut: ['g', 'd'],
+            keywords: "drafts",
+            priority: Priority.HIGH,
+            subtitle: "View your drafts",
+            section: "Navigation",
+            perform: () => {
+                setTab('drafts')
+            },
+        },
+        {
+            id: "sentAction",
+            name: "Sent",
+            shortcut: ['g', "s"],
+            keywords: "sent",
+            section: "Navigation",
+            subtitle: "View the sent",
+            perform: () => {
+                setTab('sent')
+            },
+        },
+            {
+                id: "pendingAction",
+                name: "See done",
+                shortcut: ['g', "d"],
+                keywords: "done",
+                section: "Navigation",
+                subtitle: "View the done emails",
+                perform: () => {
+                    setDone(true)
+                },
+            },
+            {
+                id: "doneAction",
+                name: "See Pending",
+                shortcut: ['g', "u"],
+                keywords: 'pending, undone, not done',
+                section: "Navigation",
+                subtitle: "View the pending emails",
+                perform: () => {
+                    setDone(false)
+                },
+        },
+    ];
+
+    return <KBarProvider actions={actions}>
             <ActualComponent>
                 {children}
             </ActualComponent>
@@ -23,6 +87,7 @@ const ActualComponent = ({children} : {children : React.ReactNode }) => {
                     <div className="border-x-0 border-b-2 dark:border-gray-700">
                         <KBarSearch className="py-4 px-6 text-lg w-full bg-white dark:bg-gray-800 outline-none border-none focus:outline-none focus:ring-0 focus:ring-offset-0" />
                     </div>
+                    <RenderResults />
                 </div>
             </KBarAnimator>
         </KBarPositioner>
