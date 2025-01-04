1f18d1ffdf8af42ac06fbb94aec0a3902a42b23b
Sushanth Nandeti <sushanthnandeti96@gmail.com>
2025-01-03 19:04:26 -0500

    openAI API integration with email context added

--- src/app/mail/ai-compose-button.tsx
+++ src/app/mail/ai-compose-button.tsx
@@ -14,6 +14,8 @@ import { Bot } from 'lucide-react'
 import { Textarea } from '@/components/ui/textarea'
 import { generateEmail } from './action'
 import { readStreamableValue } from 'ai/rsc'
+import useThreads from '@/hooks/use-threads'
+import { turndown } from '@/lib/turndown'
   
 
 type Props = {
@@ -26,19 +28,39 @@ const AiComposeButton = (props: Props) => {
 
     const [open, setOpen] = React.useState(false)
     const [prompt, setPrompt] = React.useState('')
+    const {threads, threadId, account} = useThreads()
+    const thread = threads?.find(t => t.id === threadId)
+
 
     const aiGenerate = async(prompt: string) => {
-        const {output} = await generateEmail('', prompt)
+
+        let context = ''
+
+        if(!props.isComposing) {
+            for (const email of thread?.emails?? []) {
+                const content = `
+                Subject: ${email.subject}
+                From : ${email.from}
+                Sent: ${new Date(email.sentAt).toLocaleString()}
+                Body: ${turndown.turndown(email.body ??  email.bodySnippet ?? "")}
+                }
+                `
+                context += content
+            }
+        }
+        context += `My name is ${account?.name} and my email is ${account?.emailAddress}`
+
+        const {output} = await generateEmail('context',prompt)
         for await (const token of readStreamableValue(output)) {
             if(token) {
-                console.log(token)
+                console.log(context)
                 props.onGenerate(token)
             }
         }
     }
 
     return (
-        <Dialog>
+        <Dialog open = {open} onOpenChange={setOpen}>
             <DialogTrigger>
                 <Button size='icon' variant={'outline'} onClick={() => setOpen(true)}>
                     <Bot className='size-5' />
