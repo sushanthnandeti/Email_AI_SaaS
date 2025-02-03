e9267246d4f9700b14a0a0b612228818e66c4c35
Sushanth Nandeti <sushanthnandeti96@gmail.com>
2025-02-03 16:45:03 -0500

    Disclaimer added for linking account

--- src/components/link-account-button.tsx
+++ src/components/link-account-button.tsx
@@ -1,11 +1,14 @@
-"use client"
+"use client";
 
 import { getAurinkoAuthUrl } from "@/lib/aurinko";
 import { Button } from "./ui/button";
 
 export default function LinkAccountButton() {
   return (
-    <div className="flex items-center justify-center h-screen">
+    <div className="flex flex-col items-center justify-center h-screen space-y-4">
+      <p className="text-md text-red-600 text-center max-w-md">
+        <strong>Important:</strong> Please use a <strong>Google Admin account</strong> to link your account, as Google has recently updated its terms and conditions.
+      </p>
       <Button
         className="px-6 py-3 text-lg"
         onClick={async () => {
